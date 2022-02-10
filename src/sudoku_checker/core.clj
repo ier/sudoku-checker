c739f7a72cc4f58d80e86edbd64f979b86f508ed
ier <ivan.erakhtin@gmail.com>
2022-02-11 01:11:59 +0300

    Add code and unit tests

--- src/sudoku_checker/core.clj
+++ src/sudoku_checker/core.clj
@@ -1,7 +1,73 @@
 (ns sudoku-checker.core
+  (:require
+   [clojure.string :as str]
+   [clojure.set :as set])
   (:gen-class))
 
+
+(defn- build-cols
+  [rows size]
+  (mapv
+   (fn [pos]
+     (mapv #(nth % pos) rows))
+   (range size)))
+
+
+(defn- build-boxes
+  [rows size]
+  (for [x (range size)
+        y (range size)]
+    (let [lines (->> rows
+                     (drop (* x size))
+                     (take size))
+          fnx (fn [line]
+                (take size (drop (* y size) line)))]
+      (->> lines
+           (map fnx)
+           flatten))))
+
+
+(defn- parse
+  [s box-side-size]
+  (let [line-size (* box-side-size box-side-size)
+        numbers (-> s
+                    (str/replace #"\n" " ")
+                    (str/split #" "))
+        rows (->> numbers
+                  (map #(Integer/parseInt %))
+                  (partition line-size)
+                  (map vec)
+                  vec)
+        cols (build-cols rows line-size)
+        boxes (build-boxes rows box-side-size)]
+    {:rows rows
+     :cols cols
+     :boxes boxes}))
+
+
+(defn- validate
+  [rows etalon]
+  (and
+   (every? #(= (count etalon) (count %)) (map set rows))
+   (every? empty? (map #(set/difference (set %) etalon) rows))))
+
+
+(defn check
+  [s]
+  (let [box-side-size 3
+        line-size (* box-side-size box-side-size)
+        {:keys [rows cols boxes]} (parse s box-side-size)]
+    (every?
+     #(validate % (set (range 1 (inc line-size))))
+     [rows cols boxes])))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (seq args)
+    (let [correct? (->> args first check)
+          output (if correct?
+                   "Correct sudoku input"
+                   "Incorrect sudoku input")]
+      (prn output))
+    (prn "Usage: java -jar sudoku-checker.jar \"81 digits from 1 to 9 separeated by space\"")))
--- test/sudoku_checker/core_test.clj
+++ test/sudoku_checker/core_test.clj
@@ -1,7 +1,49 @@
 (ns sudoku-checker.core-test
-  (:require [clojure.test :refer :all]
-            [sudoku-checker.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [sudoku-checker.core :refer [check]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest input-test
+  (testing "parse"
+    (let [input "1 2 3 4 5 6 7 8 9\n4 5 6 7 8 9 1 2 3\n7 8 9 1 2 3 4 5 6\n2 3 4 5 6 7 8 9 1\n5 6 7 8 9 1 2 3 4\n8 9 1 2 3 4 5 6 7\n3 4 5 6 7 8 9 1 2\n6 7 8 9 1 2 3 4 5\n9 1 2 3 4 5 6 7 8"
+          box-side-size 3
+          result (#'sudoku-checker.core/parse input box-side-size)]
+      (is (= [[1 2 3 4 5 6 7 8 9]
+              [4 5 6 7 8 9 1 2 3]
+              [7 8 9 1 2 3 4 5 6]
+              [2 3 4 5 6 7 8 9 1]
+              [5 6 7 8 9 1 2 3 4]
+              [8 9 1 2 3 4 5 6 7]
+              [3 4 5 6 7 8 9 1 2]
+              [6 7 8 9 1 2 3 4 5]
+              [9 1 2 3 4 5 6 7 8]]
+             (:rows result)))
+
+      (is (= [[1 4 7 2 5 8 3 6 9]
+              [2 5 8 3 6 9 4 7 1]
+              [3 6 9 4 7 1 5 8 2]
+              [4 7 1 5 8 2 6 9 3]
+              [5 8 2 6 9 3 7 1 4]
+              [6 9 3 7 1 4 8 2 5]
+              [7 1 4 8 2 5 9 3 6]
+              [8 2 5 9 3 6 1 4 7]
+              [9 3 6 1 4 7 2 5 8]]
+             (:cols result)))
+
+      (is (= [[1 2 3 4 5 6 7 8 9]
+              [4 5 6 7 8 9 1 2 3]
+              [7 8 9 1 2 3 4 5 6]
+              [2 3 4 5 6 7 8 9 1]
+              [5 6 7 8 9 1 2 3 4]
+              [8 9 1 2 3 4 5 6 7]
+              [3 4 5 6 7 8 9 1 2]
+              [6 7 8 9 1 2 3 4 5]
+              [9 1 2 3 4 5 6 7 8]]
+             (:boxes result)))))
+
+  (testing "check"
+    (let [correct-input "1 2 3 4 5 6 7 8 9\n4 5 6 7 8 9 1 2 3\n7 8 9 1 2 3 4 5 6\n2 3 4 5 6 7 8 9 1\n5 6 7 8 9 1 2 3 4\n8 9 1 2 3 4 5 6 7\n3 4 5 6 7 8 9 1 2\n6 7 8 9 1 2 3 4 5\n9 1 2 3 4 5 6 7 8"]
+      (is (check correct-input)))
+    
+    (let [incorrect-input "2 2 3 4 5 6 7 8 9\n4 5 6 7 8 9 1 2 3\n7 8 9 1 2 3 4 5 6\n2 3 4 5 6 7 8 9 1\n5 6 7 8 9 1 2 3 4\n8 9 1 2 3 4 5 6 7\n3 4 5 6 7 8 9 1 2\n6 7 8 9 1 2 3 4 5\n9 1 2 3 4 5 6 7 8"]
+      (is (not (check incorrect-input))))))
